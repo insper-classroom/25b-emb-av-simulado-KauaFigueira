5db53c522fbda6fef65fc83d78fa48b9f73d4af4
KauaFigueira <kauafs@al.insper.edu.br>
2025-11-09 17:45:36 -0300

    ex3 feito

--- exe3/main.c
+++ exe3/main.c
@@ -24,7 +24,10 @@ typedef struct input {
 } input_t;
 
 QueueHandle_t xQueueInput;
+QueueHandle_t xQueueLed1;
+QueueHandle_t xQueueLed2;
 
+SemaphoreHandle_t xSemaphoreLed2;
 /**
  * NÃO MEXER!
  */
@@ -35,18 +38,84 @@ void input_task(void* p) {
     test_case.num_led2 = 4;
     xQueueSend(xQueueInput, &test_case, 0);
 
-    test_case.num_led1 = 0;
+    test_case.num_led1 = 3;
     test_case.num_led2 = 2;
     xQueueSend(xQueueInput, &test_case, 0);
 
     while (true) {
-
     }
 }
 
-/**
- * Seu código vem aqui!
- */
+void main_task(void* p){
+    input_t data;
+
+    while (1){
+        if (xQueueReceive(xQueueInput, &data, pdMS_TO_TICKS(500))) {
+            xQueueSend(xQueueLed1, &(data.num_led1), 0);
+            xQueueSend(xQueueLed2, &(data.num_led2), 0);
+        }   
+    }
+}
+
+void led_1_task(void* p){
+    int data;
+    int recebeu = 0;
+    int cont = 0;
+
+    gpio_init(LED_PIN_B);
+	gpio_set_dir(LED_PIN_B, GPIO_OUT);
+
+    while (1){
+        if (!recebeu && xQueueReceive(xQueueLed1, &data, pdMS_TO_TICKS(500))) {
+            printf("rodou\n");
+            recebeu = 1;
+        }
+        
+
+        if (recebeu && cont < data) {
+            gpio_put(LED_PIN_B, 1);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            gpio_put(LED_PIN_B, 0);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            cont ++;
+        } else if (cont >= data){
+            cont = 0;
+            recebeu = 0;
+            xSemaphoreGive(xSemaphoreLed2);
+        }
+        
+    }
+}
+
+void led_2_task(void* p){
+    int data;
+    int recebeu = 0;
+    int cont = 0;
+
+    gpio_init(LED_PIN_Y);
+	gpio_set_dir(LED_PIN_Y, GPIO_OUT);
+
+    while (1){
+        if (xSemaphoreTake(xSemaphoreLed2, pdMS_TO_TICKS(500)) == pdTRUE && !recebeu && xQueueReceive(xQueueLed2, &data, pdMS_TO_TICKS(500))) {
+            printf("rodou\n");
+            recebeu = 1;
+        }
+        
+        if (recebeu && cont < data) {
+            gpio_put(LED_PIN_Y, 1);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            gpio_put(LED_PIN_Y, 0);
+            vTaskDelay(pdMS_TO_TICKS(500));
+            cont ++;
+        } else if (cont >= data){
+            cont = 0;
+            recebeu = 0;
+        }
+        
+    }
+}
+
+
 
 
 
@@ -56,8 +125,17 @@ int main() {
     /**
      * manter essas duas linhas!
      */
+
+    xSemaphoreLed2 = xSemaphoreCreateBinary();
+
     xQueueInput = xQueueCreate(32, sizeof(input_t));
+    xQueueLed1 = xQueueCreate(32, sizeof(int));
+    xQueueLed2 = xQueueCreate(32, sizeof(int));
+    
     xTaskCreate(input_task, "Input", 256, NULL, 1, NULL);
+    xTaskCreate(main_task, "Main", 256, NULL, 1, NULL);
+    xTaskCreate(led_1_task, "Led_1", 256, NULL, 1, NULL);
+    xTaskCreate(led_2_task, "Led_2", 256, NULL, 1, NULL);
 
     /**
      * Seu código vem aqui!
